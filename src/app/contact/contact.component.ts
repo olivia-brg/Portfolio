58ddd5e80bfcb2ab4aaabea4125d4a02ee5ac637
Olivia <olivia.bergaglia@gmail.com>
2024-09-28 21:50:36 +0200

    Debugged contact component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Contact } from '../models/contact';
+import { ContactService } from '../services/contact.service';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+
+export class ContactComponent implements OnInit {
+
+  contact!: Contact;
+
+  constructor(
+    private contactService: ContactService,
+  ) {};
+
+  ngOnInit(): void {
+      this.contact = this.contactService.getContacts();      
+  }
 
 }
--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -9,12 +9,11 @@ export class ContactService {
 
     private contacts: Contact = new Contact(
         `Contact`,
-        `If you'd like to get in touch, feel free to reach out through any of the following methods (due to too much cold
-        calling, please leave me a message so that I can get back to you)`,
+        `If you'd like to get in touch, feel free to reach out through any of the following methods (due to too much cold calling, please leave me a message so that I can get back to you)`,
         `olivia.bergalia@gmail.com`,
-        `+33621843088`,
-        `https://www.linkedin.com/in/olivia-bergaglia/`,
-        `https://github.com/olivia-brg`
+        `621843088`,
+        `olivia-bergaglia/`,
+        `olivia-brg`
     )
 
     getContacts(): Contact {
