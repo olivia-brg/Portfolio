fc0f75c57992e5143fe54f2192f0d8c0d3a44e62
Olivia <olivia.bergaglia@gmail.com>
2024-10-03 03:21:40 +0200

    more css

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -22,11 +22,17 @@ export class HeaderComponent implements OnInit {
       navLinks.forEach((link: HTMLAnchorElement) => {
         link.addEventListener('click', (e: MouseEvent) => {
           const target = e.target as HTMLAnchorElement;
-          const linkWidth = target.offsetWidth;
-          const linkLeft = target.offsetLeft;
+          const linkWidth = target.offsetWidth / 2;
+          const linkLeft = target.offsetLeft + linkWidth / 2;
 
+          // Appliquer la taille et la position de l'underline
           underline.style.width = `${linkWidth}px`;
           underline.style.left = `${linkLeft}px`;
+
+          // Gérer la suppression après la transition
+          setTimeout(() => {
+            underline.style.width = '0'; // Réduire à 0 après animation
+          }, 300); // 300ms correspondant à la transition CSS
         });
       });
     }
